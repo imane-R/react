76a051d3573854fcedf8e432b6a9bc494eca1cee
imane-R <imanebenamar22@gmail.com>
2024-12-17 10:18:14 +0100

    first commit

---
+++ intro-react/src/composants/containers/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div>
+      <h3>Merci de votre visite</h3>
+      <p> Mon application React</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ intro-react/src/composants/containers/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <h1>Bienvenue</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur</p>
+    </div>
+  );
+}
+
+export default Header;
