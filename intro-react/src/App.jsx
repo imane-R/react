2f032e10b4865dd127f76f024090611ef4925909
imane-R <imanebenamar22@gmail.com>
2024-12-17 15:15:05 +0100

    add player

--- intro-react/src/App.jsx
+++ intro-react/src/App.jsx
@@ -1,12 +1,13 @@
 import { Header } from "./composants/containers/Header/Header";
 import { Footer } from "./composants/containers/Footer/Footer";
 import { User } from "./composants/containers/User/User";
+import { Player } from "./composants/containers/Player/Player";
 
 function App() {
   return (
     <div>
       <Header />
-      <User />
+      <Player />
       <Footer />
     </div>
   );
--- intro-react/src/composants/containers/Footer/Footer.jsx
+++ intro-react/src/composants/containers/Footer/Footer.jsx
@@ -1,9 +1,11 @@
+import Bouton from "../../ui/Bouton/Bouton";
+
 export function Footer() {
   return (
     <footer>
       <h3>Merci de votre visite</h3>
       <p> Mon application React</p>
+      <Bouton couleur="bleu" />
     </footer>
   );
 }
-
