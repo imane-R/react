5044d26aec4fee38c5d26db9132c3d712cf51d06
imane-R <imanebenamar22@gmail.com>
2024-12-18 09:53:03 +0100

    add tailwind

--- intro-react/src/composants/ui/Bouton/Bouton.jsx
+++ intro-react/src/composants/ui/Bouton/Bouton.jsx
@@ -1,6 +1,7 @@
 export default function Bouton({ couleur }) {
   return (
     <button
+      className="rounded"
       style={{
         backgroundColor: couleur ? couleur : "black",
         color: "white",
